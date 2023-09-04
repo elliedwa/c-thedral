d7893ddb03006ba4145ad75972d7a95d92b1d10d
Elliot C. Edwards <elliot.c.edwards@gmail.com>
2023-09-04 08:53:55 -0400

    Add GPL notices to all the files

--- src/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef CATHEDRAL_UTIL_H_
-#define CATHEDRAL_UTIL_H_
-#include <limits.h>
-#include <stddef.h>
-#define VAR_TO_STR_BIN(x)                                                      \
-        obj_to_bin((char[sizeof(x) *CHAR_BIT + 1]){""}, &(x), sizeof(x))
-
-char *obj_to_bin(char *dest, const void *object, size_t osize);
-#endif
