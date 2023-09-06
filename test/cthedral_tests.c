b19f67ed415a83462819a0c21bd1d224a7b8ef57
Elliot C. Edwards <elliot.c.edwards@gmail.com>
2023-09-06 08:04:55 -0400

    chore(tests): replace tau runner with rexo.h

--- test/cthedral_tests.c
+++ test/cthedral_tests.c
@@ -1,14 +1,33 @@
-#include <tau/tau.h>
-#include <cthedral/placement_gen.h>
+#include "cthedral/bitboard.h"
+#include "cthedral/placement_gen.h"
+#include "rexo.h"
 
-
-TAU_MAIN()
-
-TEST(TestRunner, TauWorks) {
-	CHECK(1);
+RX_TEST_CASE(test_runner_works, rexo_works)
+{
+        RX_INT_REQUIRE_EQUAL(2 * 3 * 7, 42);
 }
 
-TEST(PlacementArray, TypeSize) {
-	placement_array pa;
-	CHECK(sizeof(pa)/sizeof(*pa) == 2597, "Expected placement_array to have 2597 BITBOARDs");
+RX_SET_UP(placement_array_set_up)
+{
+        placement_array *pl;
+
+        pl = (placement_array *)RX_DATA;
+        generate_placement_array(*pl);
+        return RX_SUCCESS;
+}
+
+RX_FIXTURE(placement_array_fixture, placement_array,
+           .set_up = placement_array_set_up);
+
+RX_TEST_CASE(placement_array, placement_array_size)
+{
+        placement_array *pl;
+        pl = (placement_array *)RX_DATA;
+        RX_INT_CHECK_EQUAL(2597 * sizeof(BITBOARD), sizeof(*pl));
+}
+
+int
+main(int argc, const char **argv)
+{
+        return rx_main(0, NULL, argc, argv) == RX_SUCCESS ? 0 : 1;
 }
