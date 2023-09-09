de8ee48c5293528bbefcb2acfe433bb21f82f8da
Elliot C. Edwards <elliot.c.edwards@gmail.com>
2023-09-09 09:34:32 -0400

    style(license): improve block license comments

--- src/bitboard.c
+++ src/bitboard.c
@@ -1,19 +1,29 @@
-/* Copyright 2023 Elliot C. Edwards
+/*
+ * Copyright 2023 Elliot C. Edwards
+ *
+ * c-thedral is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * c-thedral is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with c-thedral.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-c-thedral is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-c-thedral is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with c-thedral.  If not, see <https://www.gnu.org/licenses/>. */
+/*
+ * The bitboard algorithms in c-thedral are adapted from those described in
+ * "Using Bitboards for Move Generation in Shogi" by Reijer Grimbergen,
+ * available at <https://www2.teu.ac.jp/gamelab/RESEARCH/ICGAJournal2007.pdf>
+ */
 
 #include "cthedral/bitboard.h"
+
+
 
 /* this really only works one shift at a time, but that's all we need */
 PADDED_BITBOARD
